de5ce6330f1c695d6e6efe9280568afd911636d1
SeungHyun Kim <86511086+mooonu@users.noreply.github.com>
2024-04-29 22:56:13 +0900

    [Bronze II] Title: 숫자의 개수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+c = int(input())
+result = str(a * b * c)
+
+for i in range(10):
+    count = 0
+    for j in result:
+        if int(j) == i:
+            count += 1
+    print(count)
